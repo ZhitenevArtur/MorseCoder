d7c174eb953e0244f887ddc0332fd97b74086050
Lytton <40278627+Lytton612@users.noreply.github.com>
2019-01-30 15:33:47 +0300

    codeToSymb table

--- morse.js
+++ morse.js
@@ -1,14 +1,14 @@
 window.onload = function(){
 	var input = document.getElementById('inputForm');
 	input.oninput = function() {
-		document.getElementById('result').innerHTML = createMorseString(input.value);
+		document.getElementById('result').innerHTML = stringToMorse(input.value);
 	}
 }
 
-function createMorseString (textString) {
+function stringToMorse (textString) {
 	var morseString = '';
 	for (var i = 0; i < textString.length; i++) {
-		morseString+=TABLE[textString[i].toLowerCase()]
+		morseString+=TABLE_OF_SYMBOLS[textString[i].toLowerCase()]
 	}
 	return morseString;
 }
