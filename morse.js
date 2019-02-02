0092401b33537930d2d323f07e164208fd4fc21d
Lytton <40278627+Lytton612@users.noreply.github.com>
2019-02-02 03:27:57 +0300

    some fixes and both ways translations
    Fixed Morse Code table, translation from Morse to text added and some actions was made with dashes

--- morse.js
+++ morse.js
@@ -1,14 +1,45 @@
 window.onload = function(){
-	var input = document.getElementById('inputForm');
-	input.oninput = function() {
-		document.getElementById('result').innerHTML = stringToMorse(input.value);
+	document.getElementById('morseInputForm').setAttribute("placeholder", textToMorse(document.getElementById('textInputForm').getAttribute("placeholder")));
+	
+	var inputText = document.getElementById('textInputForm');
+	var inputMorse = document.getElementById('morseInputForm');
+
+	inputText.oninput = function() {
+		document.getElementById('morseInputForm').innerHTML = textToMorse(inputText.value);
+	}
+	inputMorse.oninput = function() {
+		document.getElementById('textInputForm').innerHTML = morseToText(inputMorse.value);
 	}
 }
 
-function stringToMorse (textString) {
+function textToMorse (string) {
 	var morseString = '';
-	for (var i = 0; i < textString.length; i++) {
-		morseString+=TABLE_OF_SYMBOLS[textString[i].toLowerCase()]
+	for (var i = 0; i < string.length; i++) {
+		if(!TABLE_OF_SYMBOLS[string[i].toLowerCase()]) alert(string[i] + " cannot be translated");
+		else{
+			morseString+=TABLE_OF_SYMBOLS[string[i].toLowerCase()];
+			if(i!=string.length-1) morseString+=" ";
+		}	
 	}
 	return morseString;
+}
+
+function morseToText (string) {
+	var textString = '';
+	var symbol = '';
+	for (var i = 0; i < string.length; i++) {
+		if(string[i]!=' ') symbol+=string[i];
+		if(string[i]==' ' || i == string.length-1){
+			if(!TABLE_OF_MCODES[string[i]]) alert(string[i] + " cannot be translated");
+			else{
+				textString+=TABLE_OF_MCODES[symbol];
+				symbol='';
+				if(~string.indexOf("  ", i-1)){
+					textString+=' ';
+					i++;
+				}
+			}			
+		}
+	}
+	return textString;
 }
